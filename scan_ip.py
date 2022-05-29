dd15edb0b573d200bb6da0274d66965974aca7c8
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2022-05-29 19:50:05 -0400

    Add files via upload

---
+++ scan_ip.py
@@ -0,0 +1,24 @@
+import socket
+
+# Colocar IP de acordo com o range de IP da rede (alterar apenas o valor 15 caso necessário)
+ip = '192.168.15.'
+portas = [21, 22, 23, 25, 80, 135, 8080, 443, 3306, 445]
+
+def scan():
+
+# Incrimenta o final do IP de 0 a 255
+    final_ip = list(range(255))
+    for final in final_ip:
+        ip_certo = f'{ip}{final}'
+        
+        for porta in portas:
+            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            cliente.settimeout(0.1)
+            codigo = cliente.connect_ex((ip_certo, porta))
+            
+# Printa IPs com portas da lista abertas
+            if codigo == 0:
+                print(f'Testando: {ip_certo}:{porta} -> Aberta')
+    print('Scan Finalizado!')
+
+scan()
