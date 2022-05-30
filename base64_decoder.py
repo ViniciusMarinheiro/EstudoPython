ce90e999788f7b0dc99b41d32dcbea58ada37fe6
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2022-05-29 21:20:27 -0300

    Update base64_decoder.py

--- base64_decoder.py
+++ base64_decoder.py
@@ -1,7 +1,7 @@
 import base64
 
 #
-string_codificada = 'VHVkbyBxdWUgdm9jw6ogdsOqIGRlbnRybyBkZXN0ZSBhcXVpIHRlIGxldmEgYSBjcmVyIHF1ZSB2b2PDqiBlc3TDoSBuYXZlZ2FuZG8gZW0gdW0gYXJxdWl2byAucGRmLCBwb3LDqW0gc2UgbXVkw6EtbG8gcGFyYSAudHh0IGlyw6EgcGVyY2ViZXIgcXVlIGjDoSBtYWlzIGluZm9ybWHDp8O1ZXMgYWzDqW0gZG8gcXVlIGVzdGEgdmVuZG8u'
+string_codificada = 'VmluaWNpdXMgTWFyaW5oZWlybyBEZXY='
 decodificar = base64.b64decode(string_codificada)
 
 print('=========================')
