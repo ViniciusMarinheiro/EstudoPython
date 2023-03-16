93285ff13e9b8232b3eae80698ddfa5055811e28
ViniciusMarinheiro <77128904+ViniciusMarinheiro@users.noreply.github.com>
2023-03-15 21:48:04 -0300

    Update scan_ips_com_hostname.py

--- scan_ips_com_hostname.py
+++ scan_ips_com_hostname.py
@@ -1,36 +1,34 @@
+import ipaddress
 import socket
 from timeit import default_timer as timer
 
-# Colocar IP de acordo com o range de IP da rede (alterar apenas o valor 15 caso necessário)
-ip = '192.168.15.'
+
+network = ipaddress.ip_network('192.168.15.0/24')
 portas = [21, 22, 23, 25, 80, 135, 8080, 443, 3306, 445]
 
-def scan():
 
-    # Incrimenta o final do IP de 0 a 255
-    final_ip = list(range(255))
-    for final in final_ip:
-        ip_certo = f'{ip}{final}'
+def scan():
+    for ip in network:
+        
+        
 
         for porta in portas:
             cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             cliente.settimeout(0.1)
-            codigo = cliente.connect_ex((ip_certo, porta))
+            codigo = cliente.connect_ex((str(ip), porta))
             
             
             # Printa IPs com portas da lista abertas
             if codigo == 0:
                 try:
-                    output = socket.gethostbyaddr(str(ip_certo))
+                    output = socket.gethostbyaddr(str(ip))
                     hostname = str(output).split("'")[1::2][0]
                 except:
                     hostname = 'Sem hostname'
-                print(f'Testando: {ip_certo}:{porta} -> Aberta | {hostname}')
+                print(f'Testando: {ip}:{porta} -> Aberta | {hostname}')
     print('Scan Finalizado!')
-    
 
 start = timer()
 scan()
-print('Tempo: ', timer()-start)
-
-
+tempo = timer()-start
+print(f'Tempo: {tempo:.2f} segundos')
